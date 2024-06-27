e77d0fed35459c29aee3f86b4714c3e074b99114
한경만 <kmhan@spectra.co.kr>
2024-06-28 08:04:45 +0900

    update

--- 14_agent/run_sql/main.py
+++ 14_agent/run_sql/main.py
@@ -8,23 +8,28 @@ from langchain.prompts import (
     HumanMessagePromptTemplate,
     MessagesPlaceholder,
 )
+from langchain.schema import SystemMessage
 from langchain_openai import ChatOpenAI
 
-from tools.sql import run_query_tool
+from tools.sql import run_query_tool, list_tables, describe_tables_tool
 
 # langchain.debug = True
 
 load_dotenv()
 
 chat = ChatOpenAI()
+
+tables = list_tables()
+# print(tables)
 prompt = ChatPromptTemplate(
     messages=[
+        SystemMessage("You are an AI that has access to s SQLite database.\n{tables}"),
         HumanMessagePromptTemplate.from_template("{input}"),
         MessagesPlaceholder(variable_name="agent_scratchpad"),
     ]
 )
 
-tools = [run_query_tool]
+tools = [run_query_tool, describe_tables_tool]
 agent = create_openai_functions_agent(llm=chat, tools=tools, prompt=prompt)
 agent_executor = AgentExecutor(agent=agent, verbose=True, tools=tools)
 # agent_executor.invoke({"input": "How many users are in the database?"})
